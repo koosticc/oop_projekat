542aa36be6debb7bca07676748fcb5bf5da6eec2
janoviic <janoviccc.filip@gmail.com>
2024-04-18 10:50:26 +0200

    F

--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class PointTest {
+    
     @Test
     public void test_getXTrebaDaVrati5AkoJeKlasaInstanciranaSaXjednako5() {
         Point test = new Point(5, 5);
